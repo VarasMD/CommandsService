0f82f5aef842a724a6b125aac465cf4b0244b1d5
VarasMD <md.varas.arg@gmail.com>
2025-02-02 20:27:43 -0300

    build: Se agrega nuggets y controller

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+
+            return Ok("Inbound test of from Platform Controller");
+        }
+    }
+}
